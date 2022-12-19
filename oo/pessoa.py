b9ed9b3c56dc8a1998451696b547d94e41ee260f
lokouz <fabiotakechisato@hotmail.com>
2022-12-19 11:08:47 +0900

    criado atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimenetar(self):
         return f'ola {id(self)}'
 
@@ -7,3 +11,7 @@ if __name__== '__main__':
     print(Pessoa.cumprimenetar(p))
     print(id(p))
     print(p.cumprimenetar())
+    print(p.nome)
+    p.nome = 'Fabio'
+    print(p.nome)
+    print(p.idade)
