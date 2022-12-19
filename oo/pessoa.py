ee39b1e2ed72dc89dc8eac0742325986937c8963
lokouz <fabiotakechisato@hotmail.com>
2022-12-19 11:21:47 +0900

    criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimenetar(self):
         return f'ola {id(self)}'
 
 if __name__== '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimenetar(p))
-    print(id(p))
-    print(p.cumprimenetar())
-    print(p.nome)
-    p.nome = 'Fabio'
-    print(p.nome)
-    print(p.idade)
+    takechi = Pessoa(nome='takechi')
+    fabio = Pessoa(takechi, nome='fabio')
+    print(Pessoa.cumprimenetar(fabio))
+    print(id(fabio))
+    print(fabio.cumprimenetar())
+    print(fabio.nome)
+    print(fabio.idade)
+    for filho in fabio.filhos:
+        print(filho.nome)
